ead4333e7c5f0611b46db0640969546c8bb4bed0
Chris Choi <chrischoi96@gmail.com>
2019-09-02 17:24:57 +0000

    Added random string functionality

--- express_server.js
+++ express_server.js
@@ -1,21 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const generateRandomString = () => {
+  let string = '';
+  const length = 6;
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charsLength = chars.length;
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * charsLength));
+  }
+};
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
-
-app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase }
-  res.render('urls_index', templateVars);
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -25,6 +32,20 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get('/urls', (req, res) => {
+  let templateVars = { urls: urlDatabase }
+  res.render('urls_index', templateVars);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('OK');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
 });
 
 app.listen(PORT, () => {
