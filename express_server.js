14366545a8de9420c423207d7725a5cef5571e76
Chris Choi <chrischoi96@gmail.com>
2019-09-02 17:25:40 +0000

    Added body-parser

--- express_server.js
+++ express_server.js
@@ -12,8 +12,9 @@ const generateRandomString = () => {
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const charsLength = chars.length;
   for (let i = 0; i < length; i++) {
-    result += chars.charAt(Math.floor(Math.random() * charsLength));
+    string += chars.charAt(Math.floor(Math.random() * charsLength));
   }
+  return string;
 };
 
 const urlDatabase = {
